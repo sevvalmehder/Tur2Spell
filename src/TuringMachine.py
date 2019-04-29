f36a062bf1a8ad5a56e180fdfc723ba50ae70d02
sevvalmehder <smehder@gtu.edu.tr>
2019-04-30 01:07:10 +0300

    Validation function implemented

--- src/TuringMachine.py
+++ src/TuringMachine.py
@@ -17,11 +17,25 @@ class TuringMachine(object):
         self.blank = copy.copy(blank)
         self.validation()
 
-    # NOT COMPLETED
     def validation(self):
         """Validate the Turing Machine tuples"""
+
+        # Every input symbols must be in tape symbols
+        for symbol in self.input_alphabet:
+            if symbol not in self.tape_alphabet:
+                raise ValueError("Every input symbols must be in tape symbols")
+
+        # Start state, accept state and reject state must be in states set
         if self.start_state not in self.start_state:
             raise ValueError("start_state must be in states set")
+        if self.accept_state not in self.start_state:
+            raise ValueError("accept_state must be in states set")
+        if self.reject_state not in self.start_state:
+            raise ValueError("reject_state must be in states set")
+
+        # Reject state must be different from accept state
+        if self.accept_state == self.reject_state:
+            raise ValueError("Reject state must be different from start state")
 
     def execute(self, input_tape):
         output_tape = ""
