3e3d88ee2a4e1300fdbf5a41ec397ddf28d93870
sevvalmehder <smehder@gtu.edu.tr>
2019-05-01 13:58:18 +0300

    Input control for execute function

--- src/TuringMachine.py
+++ src/TuringMachine.py
@@ -4,7 +4,8 @@ import copy
 class TuringMachine(object):
     """The definition of Turing Machine."""
 
-    def __init__(self, states, input_alphabet, tape_alphabet, blank, transitions, start_state, accept_state, reject_state):
+    def __init__(self, states, input_alphabet, tape_alphabet, blank, transitions, start_state, accept_state,
+                 reject_state):
         """Initialize a Turing Machine."""
 
         self.states = copy.copy(states)
@@ -28,8 +29,6 @@ class TuringMachine(object):
         # Start state, accept state and reject state must be in states set
         if self.start_state not in self.states:
             raise ValueError("start_state must be in states set")
-        print(self.accept_state)
-        print(self.states)
         if self.accept_state not in self.states:
             raise ValueError("accept_state must be in states set")
         if self.reject_state not in self.states:
@@ -39,6 +38,19 @@ class TuringMachine(object):
         if self.accept_state == self.reject_state:
             raise ValueError("Reject state must be different from start state")
 
+    def validate_input(self, input):
+        """"Validation function for turing machine input"""
+
+        # The last element of input must be blank symbol
+        if input[-1] != '#':
+            raise ValueError("Input must end with blank symbol({})".format(self.blank))
+
+        # Every element in input must be element of input_alphabet
+        for elm in input[:-1]:
+            if elm not in self.input_alphabet['vowels'] and elm not in self.input_alphabet['consonants']:
+                raise ValueError("{} is not in input_alphabet".format(elm))
+
     def execute(self, input_tape):
+        self.validate_input(input_tape)
         output_tape = ""
         return output_tape
